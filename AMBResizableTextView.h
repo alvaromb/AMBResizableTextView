6a4d7f893d880c0b4ad691a10208d398c6f6f69e
alvaro <alvaro@mola.com>
2014-03-17 15:06:05 +0100

    First commit

---
+++ AMBResizableTextView.h
@@ -0,0 +1,13 @@
+//
+//  AMBResizableTextView.h
+//  AMBResizableTextView
+//
+//  Created by amb on 17/03/14.
+//  Copyright (c) 2014 AMB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AMBResizableTextView : UIView
+
+@end
---
+++ AMBResizableTextView.m
@@ -0,0 +1,31 @@
+//
+//  AMBResizableTextView.m
+//  AMBResizableTextView
+//
+//  Created by amb on 17/03/14.
+//  Copyright (c) 2014 AMB. All rights reserved.
+//
+
+#import "AMBResizableTextView.h"
+
+@implementation AMBResizableTextView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
